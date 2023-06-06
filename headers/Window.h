52dc447183b58376a096c7dff36e3724543db4cf
PhoebeMitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2023-06-06 12:40:00 +0100

    First commit

---
+++ headers/Window.h
@@ -0,0 +1,14 @@
+//
+// Created by Phoebe Mitchell on 06/06/2023.
+//
+
+#ifndef SPACEINVADERS_WINDOW_H
+#define SPACEINVADERS_WINDOW_H
+
+
+class Window {
+
+};
+
+
+#endif //SPACEINVADERS_WINDOW_H
